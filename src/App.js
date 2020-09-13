8b402756f5f80b83fec37502ac931e7cbe2f585b
AlexKot322 <kotovskov.schura@yandex.ru>
2020-09-13 23:39:29 +0300

    13.09.2020

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
-    return ( <
-        div className = "app-wrapper" >
+    return ( < div className = "app-wrapper" >
         <
         Header / >
         <
-        Nav / >
-        <
-        Profile / >
+        Nav / > { /* <Profile /> */ } <
+        Dialogs / >
         <
         /div>
     );
 };
 
 export default App;
-
-// dasdsadasd
